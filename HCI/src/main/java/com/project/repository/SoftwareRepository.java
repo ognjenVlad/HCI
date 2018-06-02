a4510a6bf67f3208810aa2706e5fcb4c23356ff6
Unknown <damirhadzic996@gmail.com>
2018-06-02 20:22:29 +0200

    Reset

--- HCI/src/main/java/com/project/repository/SoftwareRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.domain.Software;
-
-public interface SoftwareRepository extends JpaRepository<Software, Long>{
-
-}
--- HCI/src/main/java/com/project/service/SoftwareService.java
+++
@@ -1,9 +0,0 @@
-package com.project.service;
-
-import com.project.domain.Software;
-
-public interface SoftwareService {
-	Software save(Software u);
-	boolean delete(Software u);
-	Software change(Software u);
-}
